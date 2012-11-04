78f4d1201d8443de8eba204038458e41c9c929fc
sevlord <sllord@mail.ru>
2012-11-04 20:54:33 +0200

    corrections

--- db/migrate/20121103110800_change_picture_file_name_field_in_clouds.rb
+++ db/migrate/20121103110800_change_picture_file_name_field_in_clouds.rb
@@ -1,11 +1,9 @@
 class ChangePictureFileNameFieldInClouds < ActiveRecord::Migration
   def up
-    rename_column :clouds, :picture_file_name, :picture_data
-    change_column :clouds, :picture_data, :binary
+    add_column :clouds, :picture_data, :binary
   end
 
   def down
-    rename_column :clouds, :picture_data, :picture_file_name
-    change_column :clouds, :picture_file_name, :string
+    remove_column :clouds, :picture_data
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103204052) do
+ActiveRecord::Schema.define(:version => 20121104123954) do
 
   create_table "avatars", :force => true do |t|
     t.binary  "facebook"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(:version => 20121103204052) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "provider"
-    t.integer  "uid"
+    t.string   "uid"
     t.string   "token"
     t.string   "secret"
   end
