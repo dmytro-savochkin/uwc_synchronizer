bb69537d4564cf93e27128552d65763ab03a5fe4
sevlord <sllord@mail.ru>
2012-11-04 14:18:57 +0200

    error solved

--- db/migrate/20121030155333_remove_cv_path_field_in_clouds.rb
+++ db/migrate/20121030155333_remove_cv_path_field_in_clouds.rb
@@ -1,11 +1,11 @@
 class RemoveCvPathFieldInClouds < ActiveRecord::Migration
   def up
     remove_column :clouds, :cv_file_name
-    remove_column :clouds, :picture_file_name_file_name
+    remove_column :clouds, :picture_file_name
   end
 
   def down
     add_column :clouds, :cv_file_name, :string
-    add_column :clouds, :picture_file_name_file_name, :string
+    add_column :clouds, :picture_file_name, :string
   end
 end
